f62380ef800a36c79ed3f4c895a57ca28d5087be
Kristina <krismarstina@gmail.com>
2022-08-26 01:27:33 +0300

    done first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const template = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}"> 
+     <img class="gallery__image" src="${preview}"  data-source="${original}" alt="${description}"/>
+     </a>`,
+  ''
+);
+
+gallery.insertAdjacentHTML('beforeend', template);
+
+let galleryLightBox = new SimpleLightbox('.gallery a');
+galleryLightBox.on('show.simplelightbox');
